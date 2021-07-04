127fda9a773004bf2d9a3f20c223cba379b8e991
i-adarsh <i.adarsh.kr@icloud.com>
2021-07-04 20:05:26 +0530

    Vehicle

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask,render_template,request,url_for
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app=Flask("My_k8s_App")
 
@@ -17,6 +20,42 @@ def kubernetes():
 @app.route("/teams")
 def team():
     return render_template("team.html")
+
+@app.route("/vehicle_detection")
+def vehicle_detection():
+    return render_template("vehicle_detection.html")
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+UPLOAD_FOLDER = '/Users/adarshkumar/Documents/Flask/Kubernetes-Python-CGI/Files'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return 'Please Select Image File Only'
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return 'No File Selected'
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template("index.html")
+#    if request.method == 'POST':
+#       f = request.files['file']
+#       f.save(secure_filename(f.filename))
+#       return 'file uploaded successfully'
 
 if __name__=="__main__":
     app.run(host = "0.0.0.0", port = 5000,debug=True)
